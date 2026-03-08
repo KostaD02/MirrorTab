0c620b0b923cda173d19ef332262072a333bda6d
kostad02 <kdautinishvili@gmail.com>
2026-03-08 19:11:49 +0400

    ci: format PR artifact name with commit count and create new comments

--- .github/scripts/append-pr-version.cjs
+++ .github/scripts/append-pr-version.cjs
@@ -2,16 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 const prNumber = process.env.PR_NUMBER;
+const prCommitCount = process.env.PR_COMMIT_COUNT;
 
-if (!prNumber) {
-  console.error('No PR_NUMBER provided.');
+if (!prNumber || !prCommitCount) {
+  console.error('Missing PR_NUMBER or PR_COMMIT_COUNT.');
   process.exit(1);
 }
 
 const packageJsonPath = path.resolve(__dirname, '../../package.json');
 const pkg = require(packageJsonPath);
 
-pkg.version = `${pkg.version}.${prNumber}`;
+pkg.version = `${pkg.version}.${prNumber}.${prCommitCount}`;
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(pkg, null, 2) + '\n');
 console.log(`Updated package version to ${pkg.version}`);
